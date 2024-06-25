d86e842cbbfb429195a6e7828832a8f4b4626252
Janvi <janvichoudhary116@gmail.com>
2024-06-25 15:47:20 +0530

    contact , testimonial, work pages

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -11,7 +11,10 @@ import 'swiper/css/pagination';
 import { Pagination } from 'swiper';
 
 //icone
-import { RxArrowTopRight } from 'react-icons/bs';
+import { BsArrowRight, RxArrowTopRight } from 'react-icons/bs';
+
+//next image
+import Image from 'next/image';
 
 // data
 const workSlider = {
@@ -62,28 +65,52 @@ const workSlider = {
 const WorkSlider = () => {
   return (
     <Swiper 
-      breakpoints ={{
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 15,
-        },
-        640: {
-          slidesPerView: 3,
-          spaceBetween: 15,
-        },
-      }}
+      spaceBetween={10}
       freeMode={true}
       pagination={{
         clickable: true,
       }}
       modules={[ Pagination]}
-      className='h-[240px] sm:h-[340px]'
+      className='h-[280px] sm:h-[480px]'
     >
       {
         workSlider.slides.map((slide,index) => {
           return (
             <SwiperSlide key={index}>
-              slide 2:50:47
+              <div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer'>
+                {slide.images.map((image, index) => {
+                  return (
+                    <div 
+                    className='relative rounded-lg overflow-hidden flex items-center justify-center group'
+                    key={index}
+                    >
+                      <div className='flex items-center justify-center relative overflow-hidden group'>
+                        {/* image */}
+                        <Image src={image.path} width={500} height={300} alt='' />
+                        {/* overlay gradient */}
+                        <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
+                        {/* title */}
+                        <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300'>
+                          <div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
+                            {/* title part1 */}
+                            <div className='delay-100'>
+                              LIVE
+                            </div>
+                            {/* title part2 */}
+                            <div  className={'translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'}>
+                              PROJECT
+                            </div>
+                            {/* icon */}
+                            <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'>
+                              <BsArrowRight />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
             </SwiperSlide>
           );
         })
