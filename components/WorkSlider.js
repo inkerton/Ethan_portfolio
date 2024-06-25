dcec34c0171e122b0551a526006049d557e57c20
Janvi <janvichoudhary116@gmail.com>
2024-06-25 12:54:59 +0530

    my works page created

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -1,5 +1,20 @@
+//import swiper react components
+import {Swiper, SwiperSlide} from 'swiper/react';
+
+//import swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+
+//import required modules
+import { Pagination } from 'swiper';
+
+//icone
+import { RxArrowTopRight } from 'react-icons/bs';
+
 // data
-const workSlides = {
+const workSlider = {
   slides: [
     {
       images: [
@@ -45,7 +60,37 @@ const workSlides = {
 };
 
 const WorkSlider = () => {
-  return <div>Work Slider</div>;
+  return (
+    <Swiper 
+      breakpoints ={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+      }}
+      freeMode={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[ Pagination]}
+      className='h-[240px] sm:h-[340px]'
+    >
+      {
+        workSlider.slides.map((slide,index) => {
+          return (
+            <SwiperSlide key={index}>
+              slide 2:50:47
+            </SwiperSlide>
+          );
+        })
+      }
+    </Swiper>
+  );
 };
 
 export default WorkSlider;
+
